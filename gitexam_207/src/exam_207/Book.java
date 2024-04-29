4c454d643ac990cb1ddda1e65392b64e7aa6ed02
Lencia Rodigues <rodrigueslencia2004@gmail.com>
2024-04-29 09:38:07 +0530

    commit1

---
+++ gitexam_207/src/exam_207/Book.java
@@ -0,0 +1,15 @@
+package exam_207;
+
+public class Book {
+	private String title;
+	private String Author;
+	public Book(String title,String Author) {
+		this.title=title;
+		this.Author=Author;
+		
+	}
+	public String getTitle() {
+		return this.title;
+	}
+
+}
